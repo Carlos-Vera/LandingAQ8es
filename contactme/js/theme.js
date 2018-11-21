0b104c961a8093f4ad646a0705eae018211b5cd6
Carlos Vera <carlosvera@veradesign.es>
2018-11-21 12:40:48 +0100

    - exteriorizar la seleccion del thema en un archivo js - Mejora de enlaces seo

---
+++ contactme/js/theme.js
@@ -0,0 +1,15 @@
+    $(function(){
+        $('.menu > a').on('click', function(e){
+            e.preventDefault();
+            $('.menu > a, .form-container').removeClass('active');
+            $(this).addClass('active');
+            $($(this).attr('href')).addClass('active');
+        });
+        $('#choose-theme > a').on('click', function(e){
+            e.preventDefault();
+            var themeName = $(this).data('theme-name');
+            $('.contactMe').removeClass('theme-minimal theme-modern theme-london').addClass(themeName);
+            $('#choose-theme > a').removeClass('active');
+            $(this).addClass('active');
+        });
+    });
