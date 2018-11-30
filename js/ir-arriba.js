d904b776a84d8dae5b667adabb9bcc35f324a18b
Carlos Vera <carlosvera@veradesign.es>
2018-11-30 17:10:39 +0100

    Error tooltip 2

--- js/ir-arriba.js
+++ js/ir-arriba.js
@@ -14,3 +14,6 @@ $("#js_up i").on('click', function (e) {
   },1400); //el valor 700 indica que lo ara en 700 mili segundos
   return false; //rompe el bucle
 });
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
